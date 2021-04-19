0b6872e1a22adf7deac5015865d1342d32a7d0fa
Maxime Belcour <maximebelcour@gmail.com>
2021-04-19 10:42:27 +0200

    Create tp4.m

---
+++ TP4/tp4.m
@@ -0,0 +1,34 @@
+clear all 
+clc
+
+
+%calculer l'intégrale analytiquement---------
+fun=@(x)exp(-x);
+q=integral(fun,0,1.2)
+
+%définir l'intervalle------------------------
+n=10
+a=0
+b=1.2
+
+[X,h] = discr_intergale(a,b,n)
+Y=exp(-X)
+
+%Q2 rectangle----------------------------------
+L1=rectangle_integrale(X,Y,h)
+
+%Q3 trapezes-----------------------------------
+L2=trapezes_integrale(a,b,n,h)
+
+%Q4 simpson-----------------------------------
+L3=simpson_integrale(a,b,n,h)
+
+%Q5 Erreur relative 
+Erelmethode1=abs(q-L1)
+Erelmethode2=abs(q-L2)
+Erelmethode3=abs(q-L3)
+
+
+
+%Plus le n est GRAND plus la précision l'erreur est faible
+%faire 3 courbes pour 3 méthodes avec n=10,15,20 
