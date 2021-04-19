103d2f46522b159a3a85ece09079fe27155bb683
Maxime Belcour <maximebelcour@gmail.com>
2021-04-19 11:20:46 +0200

    finish tp4

--- TP4/tp4.m
+++ TP4/tp4.m
@@ -2,11 +2,11 @@ clear all
 clc
 
 
-%calculer l'intégrale analytiquement---------
+%Question 1 calculer l'intégrale analytiquement---------
 fun=@(x)exp(-x);
 q=integral(fun,0,1.2)
 
-%définir l'intervalle------------------------
+%Question 2 introduction définir l'intervalle------------------------
 n=10
 a=0
 b=1.2
@@ -14,16 +14,16 @@ b=1.2
 [X,h] = discr_integrale(a,b,n)
 Y=exp(-X)
 
-%Q2 rectangle----------------------------------
+%Question 2 a) rectangle----------------------------------
 L1=rectangle_integrale(X,Y,h)
 
-%Q3 trapezes-----------------------------------
+%Question 2 b) trapezes-----------------------------------
 L2=trapezes_integrale(a,b,n,h)
 
-%Q4 simpson-----------------------------------
+%Question 2 c) simpson-----------------------------------
 L3=simpson_integrale(a,b,n,h)
 
-%Q5 Erreur relative 
+%Question 3 l'erreur relative 
 Erelmethode1=abs(q-L1)
 Erelmethode2=abs(q-L2)
 Erelmethode3=abs(q-L3)
